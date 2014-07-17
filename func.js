93fc59283abfac1d2129e5d9967a9c573d50acbf
tsuhao <tsuhao@me.com>
2014-07-16 17:43:43 -0700

    Update func.js

--- func.js
+++ func.js
@@ -1,1 +1,25 @@
-//
+var func = {
+  
+}
+
+login: function(){
+  var info ={};
+  info.id = $("input[name='username']").val();
+  info.pw = $("input[name='userpw']").val();
+  $.ajax({
+    type:"GET",
+    url:"/login",
+    data: $.param(data),
+    contentType:"application/json",
+    pass: this.onLoginpass,
+    fail: this.onLoginfail,
+    timeout: 1000
+  })
+},
+onLoginpass: function(info){
+  //Need fix
+  $.monbile.changPage('#mainPage');
+},
+onLoginfail: function(info){
+  //need fix
+},
