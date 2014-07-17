34094d5f2e467292ed0da00551de511fcbeb93b6
tsuhao <tsuhao@me.com>
2014-07-16 17:05:43 -0700

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,7 +1,18 @@
 var mongoose = require("mongoose");
 var info = requuire("./info");
 var db = mongoose.connection;
-
+var clientSchema = new mongoose.Schema({
+        clientID: String,
+        clientPW: String,
+        email: String
+});
+var appointSchema = new appointSchema({
+        doctorID: String,
+        date: Date,
+        Messages: String
+});
+var client = mongoose.model('client', clientSchema);
+var appoint = mongoose.model('appoint',appointSchema);
 db.on("error",console.error);
 db.once("open",function(){
         //
