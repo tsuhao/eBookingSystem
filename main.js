a13e75890b393d7ac195ea819ea2ab268c2a3989
tsuhao <tsuhao@me.com>
2014-07-18 21:14:19 -0700

    Update and rename requestHandlers.js to main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+var http = require("http");
+var DAO = require("DAO");
+var qs = require("querystring");
+var url = require("url");
+
+var reqID = 1;
+
+function login(dataObj){
+  if(req.method == )
